b464c8c6dae8488fa2d9d146443b158bfc424d0c
hs <freegate@wetoshare.com>
2020-08-03 16:06:32 +0900

    HelloController

---
+++ src/main/java/com/example/springbootinit/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springbootinit;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
