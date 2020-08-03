da0d99aa71337e75aa4f1e40f3634d783be853c1
hs <freegate@wetoshare.com>
2020-08-03 15:11:33 +0900

    source init

---
+++ src/main/java/com/example/springbootinit/SpringbootinitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootinitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootinit/SpringbootinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootinitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
